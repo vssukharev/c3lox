bf4d27ec0b645a06a0aa9c87c1f82f269ce32720
vssukharev <sugharevmail@gmail.com>
2024-10-18 10:23:32 +0300

    First commt

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Flake for c3-binder";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: 
+  let 
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.default = import ./shell.nix { inherit pkgs; };
+  }
+  );
+}
